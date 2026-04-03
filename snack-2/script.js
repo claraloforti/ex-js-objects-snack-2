51e196d4121428b3c3e3e75d5f0a4419919726da
claraloforti <clara.loforti@gmail.com>
2026-04-03 20:59:04 +0800

    fix snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -11,4 +11,4 @@ console.log(hamburger.ingredients[0]); // "Salad"
 console.log(secondBurger.ingredients[0]); // "Salad"
 
 // DOMANDA: Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
-// RISPOSTA: Vengono creati 2 oggetti. Viene creata una copia del primo livello dell'oggetto, ma le proprietà annidate fanno riferimento allo stesso oggetto, ciò significa che verranno modificate anche nell'originale
+// RISPOSTA: Vengono creati 3 oggetti: hamburger, ingredients e una copia del primo livello dell'oggetto hamburger, ma le proprietà annidate fanno riferimento allo stesso oggetto, ciò significa che verranno modificate anche nell'originale
